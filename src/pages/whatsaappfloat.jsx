fe725a8c74f3f12ed29af53bbfd92b2cd8793e59
challengehub-projects <greatajoku36@gmail.com>
2026-06-08 08:09:25 +0100

    Update whatsaappfloat.jsx

--- src/pages/whatsaappfloat.jsx
+++ src/pages/whatsaappfloat.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppFloat() {
   return (
     <a
-      href="https://wa.me/234XXXXXXXXXX" 
+      href="https://wa.me/+234 707 891 7208" 
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50"
